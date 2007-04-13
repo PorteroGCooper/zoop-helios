a25c97e7f69eec7d8c52d46bd1a9e9e874330d30
john <john@873ad77a-41dc-4698-a7fc-04d77048a31d>
2007-04-13 14:50:55 +0000

    small changes

--- pdf/pdf_component.php
+++ pdf/pdf_component.php
@@ -3,6 +3,7 @@
 * @category zoop
 * @package pdf
 */
+/*
 	include(dirname(__file__) . "/class.pdf.php");
 	include(dirname(__file__) . "/RotatePdf.php");
 	include(dirname(__file__) . "/pdfreport.php");
@@ -17,10 +18,29 @@
 	include(dirname(__file__) . "/PdfCircle.php");
 	include(dirname(__file__) . "/PdfLine.php");
 	//include(dirname(__file__) . "/HTMLtoPdfObject.php");
+*/
 /**
 * @package pdf
 */
 class component_pdf extends component
 {
+	function getIncludes()
+	{
+		return array(
+				"cpdf" => dirname(__file__) . "/class.pdf.php",
+				"rotatepdf" => dirname(__file__) . "/RotatePdf.php",
+				"pdfreport" => dirname(__file__) . "/pdfreport.php",
+				"pdftable_old" => dirname(__file__) . "/pdftable_old.php",
+				"pdfobject" => dirname(__file__) . "/PdfObject.php",
+				"pdfcontainer" => dirname(__file__) . "/PdfContainer.php",
+				"pdftextbox" => dirname(__file__) . "/PdfTextBox.php",
+				"pdftablerow" => dirname(__file__) . "/PdfTableRow.php",
+				"pdftablecell" => dirname(__file__) . "/PdfTableCell.php",
+				"pdftable" => dirname(__file__) . "/PdfTable.php",
+				"pdfimage" => dirname(__file__) . "/PdfImage.php",
+				"pdfcircle" => dirname(__file__) . "/PdfCircle.php",
+				"pdfline" => dirname(__file__) . "/PdfLine.php"
+		);
+	}
 }
 ?>
