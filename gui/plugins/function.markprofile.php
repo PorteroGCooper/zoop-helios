64c583c9f2a531e14e9f163ddea949dd7047a73d
steve <steve@873ad77a-41dc-4698-a7fc-04d77048a31d>
2007-04-13 16:54:36 +0000

    2 useful gui plugins

---
+++ gui/plugins/function.markprofile.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Zoop Smarty plugin
+ * @package gui
+ * @subpackage plugins
+ */
+/*
+ * Smarty plugin
+ * -------------------------------------------------------------
+ * Type:     count
+ * Name:     count
+ * Purpose:  return the size of an array
+ * -------------------------------------------------------------
+ */
+function smarty_function_markprofile($params, &$smarty)
+{
+	markprofile();
+}
+
+/* vim: set expandtab: */
+
+?>
