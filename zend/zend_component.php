df58d23cd8d00260a5b9210810ac301ddaa523bf
steve <steve@873ad77a-41dc-4698-a7fc-04d77048a31d>
2008-10-22 02:58:57 +0000

    Adding a new zend compatibility component (untested). Adding now so we can start playing with it.

---
+++ zend/zend_component.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * @category zoop
+ * @package zend
+ */
+// Copyright (c) 2008 Supernerd LLC and Contributors.
+// All Rights Reserved.
+//
+// This software is subject to the provisions of the Zope Public License,
+// Version 2.1 (ZPL). A copy of the ZPL should accompany this distribution.
+// THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+// WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+// FOR A PARTICULAR PURPOSE.
+
+/**
+ * This component will load Components From the Zend Framework.
+ *
+ * To use, define the location of the zend framework in your config at 'zoop.zend.dir'
+ * The default for this value is APP_DIR/lib/zend/library
+ * Wherever you want to use a zend component, require it,
+ *
+ * @example
+ * 	require_once('Zend/Service/Flickr.php');
+ * 	$flickr = new Zend_Service_Flickr('YOUR_FLICKR_API_KEY HERE');
+ *
+ * @package zend
+ * @uses component
+ * @version $id$
+ * @copyright 1997-2008 Supernerd LLC
+ * @author Steve Francia <webmaster@supernerd.com>
+ * @license Zope Public License (ZPL) Version 2.1 {@link http://zoopframework.com/ss.4/7/license.html}/
+ */
+class component_zend extends component {
+	function init() {
+		$dir = Config::get('zoop.zend.dir');
+
+		/**
+		 * @todo Find correct environment variable when you get off the plane.
+		 */
+		$sys = getenv('operating system');
+		if ($sys == 'Windows') {
+			ini_set('include_path', ini_get('include_path').';'. $dir );
+		} else {
+			ini_set('include_path', ini_get('include_path').':'. $dir );
+		}
+
+	}
+}
