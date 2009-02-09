6f6cafd185e7161c7f8c8915e7b591e8738ed785
justin <justin@873ad77a-41dc-4698-a7fc-04d77048a31d>
2009-02-09 22:03:08 +0000

    Moved Config class into core component.

--- core/component.php
+++ core/component.php
@@ -1,14 +1,36 @@
 <?php
 
+/**
+ * The Zoop core component.
+ * 
+ * This component includes a Config library and a set of Zoop core utils, required
+ * by the Zoop framework to run.
+ * 
+ * @extends Component
+ */
 class Component_Core extends Component {
 	function __construct() {
+		// YAML parsing library is required for Config library.
 		$this->requireComponent('spyc');
-		$this->requireComponent('config');
 		
-		//include($base . '/Config.php');
-		
+		$base = $this->getBasePath();
+		// include($base . '/Config.php');
 		//include($base . "/error.php");
 		//include($base . "/utils.php");
 		//include($base . "/FileUtils.php");
+	}
+	
+	function getIncludes() {
+		return array(
+			"config" => $this->getBasePath() . "/Config.php"
+		);
+	}
+	
+	function checkEnvironment() {
+		if (!FileUtils::isWritable(CONFIG_CACHE_DIR)) {
+			$this->envError('Unable to write to config cache file. Make sure ' . CONFIG_CACHE_DIR . ' exists and is writable.', false);
+			return false;
+		}
+		return true;
 	}
 }
